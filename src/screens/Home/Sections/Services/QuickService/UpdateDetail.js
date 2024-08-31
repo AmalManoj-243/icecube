c35fc6aa812c664e952dd882fc290c6f2eef530b
Abinashhh <vabinash44@gmail.com>
2024-08-31 12:44:31 +0530

    Error

--- src/screens/Home/Sections/Services/QuickService/UpdateDetail.js
+++ src/screens/Home/Sections/Services/QuickService/UpdateDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { StyleSheet, FlatList, View, Text, TouchableOpacity } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { SafeAreaView } from '@components/containers';
@@ -13,19 +13,18 @@ import { showToastMessage } from '@components/Toast';
 import { fetchServiceDetails } from '@api/details/detailApi';
 import { COLORS, FONT_FAMILY } from '@constants/theme';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { post } from '@api/services/utils';
+import { post, put } from '@api/services/utils'; 
 import { useAuthStore } from '@stores/auth';
 import { showToast } from '@utils/common';
 
 const UpdateDetails = ({ route, navigation }) => {
   const { id } = route.params || {};
-  console.log("🚀 ~ file: UpdateDetail.js:22 ~ UpdateDetails ~ id:", id)
   const currentUser = useAuthStore((state) => state.user);
   const [details, setDetails] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSaving, setIsSaving] = useState(false); 
   const [sparePartsItems, setSparePartsItems] = useState([]);
-  console.log("🚀 ~ file: UpdateDetail.js:25 ~ UpdateDetails ~ sparePartsItems:", JSON.stringify(sparePartsItems, null, 2))
 
   const addSpareParts = (addedItems) => {
     setSparePartsItems(prevItems => [...prevItems, addedItems]);
@@ -52,47 +51,82 @@ const UpdateDetails = ({ route, navigation }) => {
     }, [id])
   );
 
+  const handleSave = async () => {
+    setIsSaving(true);
+    const savePayload = {
+      _id: id,
+      job_stage: "In Progress",
+    };
+
+    try {
+      const response = await put(`/updateJobRegistration`, savePayload);
+      if (response.success === 'true') {
+        showToast({
+          type: "success",
+          title: "Success",
+          message: response.message || "Job registration updated successfully",
+        });
+      } else {
+        console.error("Save Failed:", response.message);
+        showToast({
+          type: "error",
+          title: "ERROR",
+          message: response.message || "Job registration update failed",
+        });
+      }
+    } catch (error) {
+      console.error("Error Saving Job Registration:", error);
+      showToast({
+        type: "error",
+        title: "ERROR",
+        message: "An unexpected error occurred. Please try again later.",
+      });
+    } finally {
+      setIsSaving(false);
+    }
+  };
+
   const handleSubmit = async () => {
     setIsSubmitting(true);
-    console.log(hii);
-    
     const requestPayload = {
-      job_registration_id: id,
-      proposed_action_id: null,
-      proposed_action_name: null,
-      status: "waiting for parts",
-      created_by: null,
-      created_by_name: "",
-      assigned_to: "6650704c2e5cf73d84470013",
-      assigned_to_name: "Abhijith Danat",
-      warehouse_id: "66307fc0ceb8eb834bb25509",
-      warehouse_name: "Danat Hub",
-      sales_person_id: null,
-      sales_person_name: "",
-      done_by_id: currentUser?.related_profile?._id || null,
-      done_by_name: currentUser?.related_profile?.name || '',
-      parts_or_service_required: null,
-      service_type: null,
-      untaxed_total_amount: 12,  //
-      service_charge: 100,  //
-      total_amount: 112.6,  //
-      parts: sparePartsItems.map((items) => ({
-        product_id: items?.product.id,
-        product_name: items?.product.label,
-        description: items?.description,
-        uom_id: items?.uom?.id,
-        uom: items?.uom.label,
-        unit_price: items.unitPrice,
-        unit_cost: '',
-        tax_type_name: "vat 5%",
-        tax_type_id: "648d9b54ef9cd868dfbfa37b"
-      }))
-    }
+      _id: id,
+      job_stage: "Waiting for spare",
+      create_job_diagnosis: [
+        {
+          job_registration_id: id,
+          proposed_action_id: null,
+          proposed_action_name: null,
+          done_by_id: currentUser?.related_profile?._id || null,
+          done_by_name: currentUser?.related_profile?.name || '',
+          // assigned_to: "6650704c2e5cf73d84470013",
+          // assigned_to_name: "Abhijith Danat",
+          // warehouse_id: currentUser?.warehouse?.warehouse_id,
+          // warehouse_name: currentUser?.warehouse?.warehouse_name,
+          untaxed_total_amount: 12, //
+          parts_or_service_required: null,
+          service_type: null,
+          service_charge: items?.serviceCharge,  
+          total_amount: items?.totalAmount,  
+          parts: sparePartsItems.map((items) => ({
+            product_id: items?.product?.id,
+            product_name: items?.product?.label,
+            description: items?.description,
+            uom_id: items?.uom?.id,
+            uom: items?.uom?.label,
+            unit_price: items?.unitPrice,
+            tax_type_id: items?.tax?.id,
+            tax_type_name: items?.tax?.label,
+            quantity: items?.quantity,
+            sub_total: items?.subTotal,
+            unit_cost: items?.totalAmount,
+          }))
+        }
+      ]
+    };
+    console.log("🚀 ~ file: UpdateDetail.js:25 ~ requestPayload ~ requestPayload:", JSON.stringify(requestPayload, null, 2))
 
-    console.log("🚀 ~ file: UpdateDetail.js:86 ~ handleSubmit ~ requestPayload:", JSON.stringify(requestPayload, null, 2))
     try {
       const response = await post("/createJobApproveQuote", requestPayload);
-      console.log("🚀 ~ submit ~ response:", response);
       if (response.success === 'true') {
         showToast({
           type: "success",
@@ -101,15 +135,15 @@ const UpdateDetails = ({ route, navigation }) => {
         });
         navigation.navigate("QuickServiceScreen");
       } else {
-        console.error("Spare Parts Request Failed:", response.message);
+        console.error("Submit Failed:", response.message);
         showToast({
           type: "error",
           title: "ERROR",
-          message: response.message || "Spare Parts Request updation failed",
+          message: response.message || "Spare Parts Request update failed",
         });
       }
     } catch (error) {
-      console.error("Error Updating Spare Parts Request Failed:", error);
+      console.error("Error Submitting Spare Parts Request:", error);
       showToast({
         type: "error",
         title: "ERROR",
@@ -156,12 +190,21 @@ const UpdateDetails = ({ route, navigation }) => {
           keyExtractor={(item, index) => index.toString()}
         />
         <Button
+          title={'SAVE'}
+          width={'50%'}
+          alignSelf={'center'}
+          backgroundColor={isSaving ? COLORS.gray : COLORS.primaryThemeColor}
+          onPress={handleSave}
+          disabled={isSaving || isSubmitting}
+        />
+        <Button
           title={'SUBMIT'}
           width={'50%'}
           alignSelf={'center'}
-          backgroundColor={COLORS.primaryThemeColor}
+          backgroundColor={COLORS.orange}
           onPress={handleSubmit}
         />
+
       </RoundedScrollContainer>
       {isLoading && <OverlayLoader />}
     </SafeAreaView>
